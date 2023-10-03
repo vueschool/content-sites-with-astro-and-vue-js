80e53eee0ebd277f1bb1ae2daa764ddb94e1f060
Daniel Kelly <me@danielkelly.io>
2023-10-03 13:38:20 -0500

    6-how-to-pass-astro-data-to-your-vue-components

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,7 +1,12 @@
 <script setup>
 import { ref } from "vue";
-const counter = ref(0);
+const props = defineProps({
+  initialCount: Number,
+});
+const counter = ref(props.initialCount ?? 0);
 </script>
 <template>
+  <slot name="top"></slot>
   <button @click="counter++">{{ counter }}</button>
+  <slot name="bottom"></slot>
 </template>
